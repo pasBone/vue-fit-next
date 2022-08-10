25384a90e2988e6587cf492c143fea0185c17049
pasBone <pasBone2@gmail.com>
2022-08-10 17:37:37 +0800

    feat: 添加 leave hook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { App, Plugin } from 'vue'
 import type { FitOptions } from './types'
 import { directiveHooks } from './fit'
+export { leave } from './animations'
 
 /**
  * The vue directive.
