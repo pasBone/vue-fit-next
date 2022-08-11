9871a813cd1102f4c6520849924d57dad111711b
pasBone <pasBone2@gmail.com>
2022-08-11 17:46:10 +0800

    chore: 尝试新的方案

---
+++ playground/src/components/Header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-fit:2 class="header">
+    <div class="header-bg" />
+  </div>
+</template>
+
+<style scoped>
+.header {
+  width: 3840px;
+  height: 119px;
+}
+
+.header-bg{
+  width: 3804px;
+  height: 119px;
+  margin: 0 auto;
+  background: url("/header-bg.png");
+  background-size: 100% 100%;
+}
+</style>
