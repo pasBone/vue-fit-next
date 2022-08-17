927d104dca812d6c200607420e5b3d08bc951b8f
pasBone <pasBone2@gmail.com>
2022-08-17 15:10:19 +0800

    feat: 添加方法getComputedStyleNumber

--- src/utils.ts
+++ src/utils.ts
@@ -9,10 +9,15 @@ export function getTranslateValue(el: HTMLElement) {
 /** 生成 nanoid */
 export function nanoId(size: number) {
   const urlAlphabet
-  = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
+    = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
   let id = ''
   let i = size
   while (i--)
     id += urlAlphabet[(Math.random() * 64) | 0]
   return id
+}
+
+/** 获取css 属性值 */
+export function getComputedStyleNumber(el: HTMLElement, attr: string) {
+  return parseFloat(getComputedStyle(el, null).getPropertyValue(attr) || '0')
 }
